7933e2b14b92d2390fc89fc7d5a766c8ca92b8f4
choco leung <sendtochoco@gmail.com>
2017-08-10 15:38:30 +0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+ root "cocktails#index"
+
+ delete 'doses/:id', to: "doses#destroy"
+  resources :cocktails do
+    resources :doses
+  # , only: [:show, :index, :new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
